6584ec8f5bb0fc8a221f29c06cb1eaecb86c90dd
bbecquet <benjamin.becquet@gmail.com>
2015-11-20 17:40:03 +0100

    Replace old RotatedMarker code by submodule to new repo

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('concat', function(){
-  return gulp.src('src/*.js')
+  return gulp.src(['src/*.js', 'Leaflet.RotatedMarker/leaflet.rotatedMarker.js'])
     .pipe(concat('leaflet.polylineDecorator.js'))
     .pipe(gulp.dest('.'));
 });
